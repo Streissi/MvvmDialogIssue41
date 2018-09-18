808757528623116386f910ed1000f37d3c8d63fc
dstreisselberger <dstreissi@gmail.com>
2018-09-18 18:23:32 +0200

    Initial commit

---
+++ Bootstrapper.cs
@@ -0,0 +1,43 @@
+﻿using MvvmDialogs;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MvvmDialogIssue41.Dialogs;
+
+namespace MvvmDialogIssue41
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window)this.Shell;
+
+            App.Current.MainWindow.Show();
+
+            var dialogService = Container.Resolve<IDialogService>();
+            if (dialogService != null)
+            {
+                var mainViewModel = ((Window)Shell).DataContext as MainWindowViewModel;
+                var dialogModel = Container.Resolve<InfoViewModel>();
+                dialogService.ShowDialog(mainViewModel, dialogModel);
+            }
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            Container.RegisterInstance<IDialogService>(new DialogService(), new ContainerControlledLifetimeManager());
+        }
+    }
+}
---
+++ Dialogs/InfoViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MvvmDialogs;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmDialogIssue41.Dialogs
+{
+    public class InfoViewModel : BindableBase, IModalDialogViewModel
+    {
+        private string _text;
+        private bool? _dialogResult;
+
+        public InfoViewModel()
+        {
+            Text = "INFO DIALOG";
+            OkCommand = new DelegateCommand(OnOk);
+        }
+
+        private void OnOk()
+        {
+            DialogResult = true;
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set { SetProperty(ref _text, value); }
+        }
+
+        public DelegateCommand OkCommand { get; }
+
+        public bool? DialogResult {
+            get { return _dialogResult; }
+            set { SetProperty(ref _dialogResult, value); }
+        }
+    }
+}
